bc2f8d0cfd6fd541206cb53346c8f6f3246ce5dd
Spenser Hale <spenserhale@outlook.com>
2014-10-25 18:03:36 -0700

    -Ported over existing header and footer into theme -added css and js

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer-clear"></div>
+<footer class="row no-max pad">
+	<ul class="social-links no-bullet">
+	</ul>
+	<p>Copyright <?php echo date('Y'); ?></p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
