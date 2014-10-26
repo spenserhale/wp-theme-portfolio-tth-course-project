65d89b0b16b8d81d22a3207f2fcff7090bf81a22
Spenser Hale <spenserhale@outlook.com>
2014-10-25 20:43:04 -0700

    added article formatting for blog

--- home.php
+++ home.php
@@ -7,16 +7,25 @@
 				<div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
 					<div class="primary">
 
-						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-							<h1><?php the_title(); ?></h1>
-							<p><?php the_content(); ?></p>
-
-						<?php endwhile; else : ?>
-
-							<p><?php _e( 'Sorry, no pages found.' ); ?></p>
-
-						<?php endif; ?>
+						<article <?php post_class('post'); ?>>
+							<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+							<h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+							<ul class="post-meta no-bullet">
+								<li class="author">
+			                      <span class="wpt-avatar small">
+			                        <?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
+			                      </span>
+									by <?php the_author_posts_link(); ?>
+								</li>
+								<li class="cat">in <?php the_category( ', ' ); ?></li>
+								<li class="date">on <?php the_time('F j, Y'); ?></li>
+							</ul>
+							<?php if( get_the_post_thumbnail() ) : ?>
+								<div class="img-container">
+									<?php the_post_thumbnail('large'); ?>
+								</div>
+							<?php endif; ?>
+						</article>
 
 					</div>
 				</div>
