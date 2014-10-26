d426ce6d401fbd051ae06959e54aed7dd852c2e3
Spenser Hale <spenserhale@outlook.com>
2014-10-25 17:50:08 -0700

    -moved js -added enqueues for js in functions -made js dependency checks -added jQuery noConflict wrapper to app.js

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-$( ".nav-toggle" ).click(function() {
-  $(this).toggleClass("open");
-  $("nav").fadeToggle(100);
-
-  return false;
-});
--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php
 
-function wpt_theme_styles() {
-	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundations.css');
-	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css');
-	wp_enqueue_style( 'google_font_asap_css', '//fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic');
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
+function wpt_theme_scriptswpt_theme_styles() {
+	wp_enqueue_style('foundation_css', get_template_directory_uri() . '/css/foundations.css');
+	wp_enqueue_style('normalize_css', get_template_directory_uri() . '/css/normalize.css');
+	wp_enqueue_style('google_font_asap_css', '//fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic');
+	wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css');
 
 }
 
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles');
+
+function wpt_theme_scripts() {
+	wp_enqueue_script('modernizer_js', get_template_directory_uri() . '/js/modernizer.js', '', '', false);
+	wp_enqueue_script('foundation_js', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '', true);
+	wp_enqueue_script('foundation_js', get_template_directory_uri() . '/js/foundation.js', array('jquery', 'foundation_js'), '', true);
+}
+
+add_action( 'wp_enqueue_scripts', 'wpt_theme_scripts');
